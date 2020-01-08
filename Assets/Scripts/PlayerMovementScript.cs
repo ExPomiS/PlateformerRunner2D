b11c144f1188807be242a031216b19bd587a0255
Oui <suickev@hotmail.fr>
2020-01-08 17:28:55 +0100

    INIT JUMP

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -7,25 +7,48 @@ public class PlayerMovementScript : MonoBehaviour
     [Header("Vitesse")] public float speed;
     private float _maxSpeed;
 
+    [Header("Saut")] [Range(0.1f, 1f, order = 1)]
+    public float jumpMultiplier = 1f;
+
     [Header("Rigidbody")] public Rigidbody2D playerRb;
-    
+
+    [Header("DétectionSol")] public bool isGrounded;
+    public LayerMask GroundLayer;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         _maxSpeed = speed;
         playerRb.AddRelativeForce(new Vector2(speed, 0), ForceMode2D.Impulse);
     }
-    
+
     void Update()
     {
+        //----------------------------------------------------------------------------------
         if (playerRb.velocity.magnitude <= _maxSpeed)
         {
             playerRb.AddRelativeForce(new Vector2(speed, 0), ForceMode2D.Force);
         }
 
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0 && isGrounded)
         {
-            playerRb.AddForce(Vector2.up);
+            playerRb.AddForce(Vector2.up * jumpMultiplier, ForceMode2D.Impulse);
+        }
+        //----------------------------------------------------------------------------------
+        RaycastDetection();
+    }
+
+    void RaycastDetection()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 0.75f, GroundLayer);
+        Debug.DrawRay(this.transform.position, Vector2.down, Color.green);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }
